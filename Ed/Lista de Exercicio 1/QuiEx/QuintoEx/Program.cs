149a1cab05a9bceb364def4a25c356cebd6c59e8
Galdinowski <arthurlazarogaldino@gmail.com>
2025-03-24 13:19:40 -0300

    Adicionando a pasta ED com a primeira lista de exercicios sobre recursividade

---
+++ Ed/Lista de Exercicio 1/QuiEx/QuintoEx/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+
+
+class program
+{  
+    static double juros;
+    static int meses;
+
+    static void Aposentadoria( double montante, double retirada)
+    {  
+        if(montante < retirada )
+        {
+             Console.WriteLine($"O total de meses disponiveis para a retirada é de : {meses}") ;
+             return;
+        }
+
+        
+            montante = (montante+(montante*juros))- retirada;
+            meses++;
+            
+            Console.WriteLine(montante.ToString("C2",new CultureInfo("pt-BR")));
+
+             Aposentadoria(montante , retirada);
+
+
+    }
+    static void Main(string[] args)
+    {
+        double montante = 100000.50;
+    
+        double retirada = 10000.50;
+        meses = 0;
+        juros = 0.0055;
+         Aposentadoria(montante,retirada);
+         
+
+    }
+
+
+
+}
---
+++ Ed/Lista de Exercicio 1/SexEx/SextoEx/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static int tamanho ;
+    static int contador;
+    static string vogais ="aeiouAEIOU";
+
+
+    static void Vogais (string vog)
+    {
+        
+        if(tamanho < 0)
+        {
+            
+            Console.WriteLine("");
+             Console.WriteLine($"O numero de vogais nesta Palavras é de: {contador}");
+            return;
+        }
+
+    
+
+            if(vogais.Contains(vog[tamanho]))
+            {
+                
+                contador ++; 
+                
+                Console.Write($"[{vog[tamanho]}] ");
+               
+            }
+            
+
+             tamanho --;
+              
+              Vogais(vog);
+               
+                
+                 
+    }
+
+
+    static void Main(string[] args)
+    {
+        string vog = "carnavalesco";
+        tamanho = vog.Length-1;
+        contador = 0;
+        Console.Write("As vogais presentes sao : ");
+        Vogais(vog);
+
+    }
+
+
+}
+
