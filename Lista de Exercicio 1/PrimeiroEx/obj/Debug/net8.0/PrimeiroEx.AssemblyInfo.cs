ae4ce0ff80139d1a568480439069936ba652e270
Galdinowski <arthurlazarogaldino@gmail.com>
2025-04-14 15:31:48 -0300

    Your commit message

--- Lista de Exercicio 1/PrimeiroEx/obj/Debug/net8.0/PrimeiroEx.AssemblyInfo.cs
+++ Lista de Exercicio 1/PrimeiroEx/obj/Debug/net8.0/PrimeiroEx.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroEx")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22800e4ebfee4f251556290ca9d7261da2e1139e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f4050e79cc2c133e3aa1fa354f95b7bec78994")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroEx")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PrimeiroEx")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
